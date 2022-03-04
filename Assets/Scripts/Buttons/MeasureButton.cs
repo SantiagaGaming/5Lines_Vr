aa6dcd7f147351cd937351d902b23a6fbea5fa9b
Ilya <ganychev92@gmail.com>
2022-03-04 11:17:16 +0300

    Buttons scale

--- Assets/Scripts/Buttons/MeasureButton.cs
+++ Assets/Scripts/Buttons/MeasureButton.cs
@@ -5,13 +5,23 @@ using AosSdk.Core.Player;
 using AosSdk.Core.Player.Pointer;
 using UnityEngine;
 using UnityEngine.Events;
-public class MeasureButton : MonoBehaviour, IClickAble
+public class MeasureButton : MonoBehaviour, IClickAble, IHoverAble
 {
     public UnityAction<Transform, string> MeasureButtonClickEvent;
+    public bool IsClickable { get; set; } = true;
+    public bool IsHoverable { get; set; } = true;
     public virtual void OnClicked(InteractHand interactHand)
     {
         MeasureButtonClickEvent?.Invoke(transform,gameObject.name);
     }
 
-    public bool IsClickable { get; set; } = true;
+    public void OnHoverIn(InteractHand interactHand)
+    {
+        transform.localScale *= 2;
+    }
+    public void OnHoverOut(InteractHand interactHand)
+    {
+        transform.localScale /= 2;
+    }
+
 }
