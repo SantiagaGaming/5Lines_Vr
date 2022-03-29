b6bc2fbb95f422fb3bb6f88b8479f5ab1014b2ae
Ilya <ganychev92@gmail.com>
2022-03-29 14:38:05 +0300

    Oscil object and scripts

---
+++ Assets/Scripts/Buttons/OscilLeverArmButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using AosSdk.Core.Interfaces;
+using AosSdk.Core.Utils;
+using AosSdk.Core.Player;
+using AosSdk.Core.Player.Pointer;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class OscilLeverArmButton : BaseButtton
+{
+    [SerializeField] private bool _left;
+    [SerializeField] private GameObject _leverArm;
+    [SerializeField] private GameObject _strelka;
+
+    public override void OnClicked(InteractHand interactHand)
+    {
+        if(_left)
+        {
+            _leverArm.transform.localRotation *= Quaternion.Euler(0, -15f, 0);
+        }
+        else
+        {
+            _leverArm.transform.localRotation *= Quaternion.Euler(0, 15f, 0);
+        }
+    }
+}
