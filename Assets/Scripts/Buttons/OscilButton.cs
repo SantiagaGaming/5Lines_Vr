73ff55c4d6647902f8cabee9f61c2c60ace4114b
Ilya <ganychev92@gmail.com>
2022-04-04 16:36:13 +0300

    Ampermetr added

--- Assets/Scripts/Buttons/OscilButton.cs
+++ Assets/Scripts/Buttons/OscilButton.cs
@@ -30,18 +30,18 @@ public class OscilButton : BaseButtton
             GetComponent<Image>().enabled = false;
             ButtonDownEvent?.Invoke(_index);
        
-            while(_buttonBox.transform.localPosition.y>=0.45f)
+            while(_buttonBox.transform.localPosition.y>=0.059f)
             {
-                _buttonBox.transform.localPosition -= new Vector3(0, 0.005f, 0);
+                _buttonBox.transform.localPosition -= new Vector3(0, 0.001f, 0);
                 yield return new WaitForSeconds(0.005f);
             }
         }
         else
         {
             yield return new WaitForSeconds(0.05f);
-            while (_buttonBox.transform.localPosition.y <= 0.55f)
+            while (_buttonBox.transform.localPosition.y <= 0.065f)
             {
-                _buttonBox.transform.localPosition += new Vector3(0, 0.005f, 0);
+                _buttonBox.transform.localPosition += new Vector3(0, 0.001f, 0);
                 yield return new WaitForSeconds(0.005f);
             }
             GetComponent<Collider>().enabled = true;
