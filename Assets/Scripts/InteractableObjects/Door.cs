c9a0dc5c7529af84bd3d86936fc6d6300f9f01dd
SantiagaGaming <ganychev92@gmail.com>
2022-04-06 18:04:00 +0300

    Doors logic added

--- Assets/Scripts/InteractableObjects/Door.cs
+++ Assets/Scripts/InteractableObjects/Door.cs
@@ -6,10 +6,13 @@ using AosSdk.Core.Player.Pointer;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Door : MonoBehaviour, IClickAble
+public class Door : MonoBehaviour, IClickAble, IHoverAble
 {
     public UnityAction<Transform> TeleportToObjectEvent;
     public UnityAction AosTeleportEvent;
+    public bool IsHoverable { get; set; } = true;
+
+    public bool IsClickable { get; set; } = true;
 
     [SerializeField] private Transform _newPlayerPosition;
     public void StartTeleporting()
@@ -21,7 +24,26 @@ public class Door : MonoBehaviour, IClickAble
     {
         StartTeleporting();
     }
-    public bool IsHoverable { get; set; } = true;
 
-    public bool IsClickable { get; set; } = true;
+    public virtual void OnHoverIn(InteractHand interactHand)
+    {
+        if (GetComponent<Renderer>())
+            GetComponent<Renderer>().material.color *= 1.5f;
+        else if (GetComponentInChildren<Renderer>())
+            GetComponentInChildren<Renderer>().material.color *= 1.5f;
+        else if (GetComponentInParent<Renderer>())
+            GetComponentInParent<Renderer>().material.color *= 1.5f;
+        else return;
+
+    }
+    public virtual void OnHoverOut(InteractHand interactHand)
+    {
+        if (GetComponent<Renderer>())
+            GetComponent<Renderer>().material.color /= 1.5f;
+        else if (GetComponentInChildren<Renderer>())
+            GetComponentInChildren<Renderer>().material.color /= 1.5f;
+        else if (GetComponentInParent<Renderer>())
+            GetComponentInParent<Renderer>().material.color /= 1.5f;
+        else return;
+    }
 }
